b5ef7834d1c2286f04ab054785919835ef402bfa
Mugwaneza@2005fati8 <mugwanezahakimu@gmail.com>
2025-09-18 19:26:03 +0200

    v0.0.1

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,7 +1,6 @@
 import { authStore } from '$lib/stores/auth';
 import type { Load } from '@sveltejs/kit';
 
-
 export const load: Load  = async ({ fetch }) => {
     const res = await fetch(`/api/auth/check-auth`, {
         method: 'POST',
